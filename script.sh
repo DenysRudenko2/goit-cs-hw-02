9af8bfef58c028355e3b42c7cfc7df4ff7ac38f0
drudenko <denisrudenko1986@gmail.com>
2024-11-17 17:32:50 +0200

    hw02 task 1 - 2

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+websites=(
+  "https://github.com"
+  "https://www.freecodecamp.org"
+  "https://www.codewars.com"
+  "https://copilot.microsoft.com"
+  "https://chatgpt.com"
+  "https://chatgptasfasfasf.com"
+)
+
+log_file="site_status.log"
+
+: > "$log_file"
+available=0
+
+for website in "${websites[@]}"; do
+  # Отримання статусу доступності через curl
+  if curl -o /dev/null -s -w "%{http_code}" -L "$website" | grep -q "^200$"; then
+    available=$((available+1))
+    echo "$website доступний" >> "$log_file"
+  else
+    echo "$website не доступний" >> "$log_file"
+  fi
+done
+
+now=$(date)
+echo "вебсайти кількістю $available із загальної кількості, яка дорівнює ${#websites[@]} на момент часу ${now} є доступними. "
+echo "Результати перевірки збережено у файл: $log_file"
